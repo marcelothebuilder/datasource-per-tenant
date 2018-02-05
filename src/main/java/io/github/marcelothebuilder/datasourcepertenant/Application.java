3342110b6e878c54bd58d0418bcb71136ed65196
Marcelo Paixão Resende <marcelopaixaoresende@gmail.com>
2018-02-05 21:19:55 -0200

    enable spring transaction

--- src/main/java/io/github/marcelothebuilder/datasourcepertenant/Application.java
+++ src/main/java/io/github/marcelothebuilder/datasourcepertenant/Application.java
@@ -4,9 +4,11 @@ import io.github.marcelothebuilder.datasourcepertenant.repository.config.Company
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = CompanyRepository.class)
+@EnableTransactionManagement
 public class Application {
     public static void main(String[] main) {
         SpringApplication.run(Application.class, main);
